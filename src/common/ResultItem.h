0cf3274b9ffb87536721a6caaae168c63cc43cb3
georgel2020 <lingeorge2020@outlook.com>
2025-08-16 21:14:47 +0800

    Implement launcher functionality and UI
    - Upgrade C++ standard to C++20 in CMake configuration
    - Remove `.clang-format`
    - Replace hardcoded search icon with Material Symbols font
    - Implement `ResultItemDelegate` for custom result item rendering
    - Add `Action` and `ResultItem` structures for encapsulating result data and actions
    - Enable keyboard navigation and action execution in results list
    - Use a Qt resource file for embedding assets
    - Refactor `setupUi` in `Launcher` to support updated components and layouts

---
+++ src/common/ResultItem.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Action.h"
+#include <QString>
+#include <QVector>
+
+/**
+ * @struct ResultItem
+ * @brief Represent an item in a result view.
+ *
+ * Members:
+ *
+ * - title: The main title representing the item.
+ * - subtitle: A secondary description providing additional context.
+ * - iconGlyph: A single character representing an icon in glyph form.
+ * - iconPath: The file path to an external icon source, such as an executable file.
+ * - actions: A collection of actions that the user can perform on this item.
+ *
+ * An icon must be provided, either as a font icon or a path.
+ */
+struct ResultItem
+{
+    QString title;
+    QString subtitle;
+    QChar iconGlyph;
+    QString iconPath;
+    QVector<Action> actions;
+};
+
+Q_DECLARE_METATYPE(ResultItem)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "app/Launcher.h"
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+
+    // Load Font Awesome icon font.
+    QFontDatabase::addApplicationFont(":/fonts/MaterialSymbolsRounded-Regular.ttf");
+
+    // Create main window.
     Launcher w;
     return QApplication::exec();
 }
