3b8d05ce08695dec12d8716548147055cf291cd6
georgel2020 <86717650+georgel2020@users.noreply.github.com>
2025-08-17 22:12:13 +0800

    Add Launcher configuration loading support
    - Introduce `ConfigLoader::loadConfig` to load and save configuration for `Launcher`
    - Add `defaultConfig` method in `Launcher` to return default configuration
    - Modify `Launcher` to load configuration on initialization

--- src/core/ConfigLoader.cpp
+++ src/core/ConfigLoader.cpp
@@ -4,7 +4,40 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QStandardPaths>
+#include "../app/Launcher.h"
 #include "../modules/IModule.h"
+
+/**
+ * Load configuration file for Launcher.
+ *
+ * If the file does not exist, a default configuration will be applied.
+ *
+ * @param launcher A pointer to Launcher main window.
+ * @return The QJsonDocument representing the configuration.
+ */
+QJsonDocument ConfigLoader::loadConfig(const Launcher* launcher)
+{
+    QFile file(getConfigPath());
+
+    if (file.exists())
+    {
+        file.open(QIODevice::ReadOnly | QIODevice::Text);
+        const QByteArray data = file.readAll();
+        file.close();
+        QJsonParseError error;
+        const QJsonDocument doc = QJsonDocument::fromJson(data, &error);
+
+        if (error.error != QJsonParseError::NoError)
+            return {};
+
+        return doc;
+    }
+    const QJsonDocument doc = launcher->defaultConfig();
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    file.write(doc.toJson(QJsonDocument::Indented));
+    file.close();
+    return doc;
+}
 
 /**
  * Load configuration file for a Launcher module.
@@ -39,6 +72,26 @@ QJsonDocument ConfigLoader::loadModuleConfig(const IModule* module)
     file.write(doc.toJson(QJsonDocument::Indented));
     file.close();
     return doc;
+}
+
+/**
+ * Get the configuration file path for Launcher.
+ *
+ * If the path does not exist, create the folders automatically.
+ *
+ * @return The file path string.
+ */
+QString ConfigLoader::getConfigPath()
+{
+    const QString configDir = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
+    if (configDir.isEmpty())
+        return {};
+
+    const QDir dir(configDir);
+    if (!dir.exists())
+        dir.mkpath(".");
+
+    return dir.filePath("Launcher.json");
 }
 
 /**
