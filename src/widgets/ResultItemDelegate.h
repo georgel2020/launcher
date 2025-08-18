90392307d42885e364edafa4ffac6768aa33b9a3
georgel2020 <86717650+georgel2020@users.noreply.github.com>
2025-08-18 13:00:58 +0800

    Support mouse navigation
    - Extend `ResultItemDelegate` to support mouse hover effects
    - Highlight hovered items and show action buttons dynamically
    - Implement `editorEvent` to handle interaction with action buttons and result items
    - Refactor action button rendering logic to handle hover and selection states
    - Enable mouse tracking for results list to support hover detection
    - Emit `hideWindow` signal from `ResultItemDelegate` on action execution and connect it in `Launcher`

--- src/widgets/ResultItemDelegate.h
+++ src/widgets/ResultItemDelegate.h
@@ -10,26 +10,34 @@ class ResultItemDelegate final : public QStyledItemDelegate
     Q_OBJECT
 
 public:
-    explicit ResultItemDelegate(QObject* parent = nullptr);
+    explicit ResultItemDelegate(QAbstractItemView* view, QObject* parent = nullptr);
 
     void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
-
     [[nodiscard]] QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+    bool editorEvent(QEvent* event, QAbstractItemModel* model, const QStyleOptionViewItem& option, const QModelIndex& index) override;
 
     [[nodiscard]] int getCurrentActionIndex() const;
-    void setCurrentActionIndex(int index);
+    void setCurrentActionIndex(int index) const;
 
 private:
     static void drawIcon(QPainter* painter, const QRect& rect, const QIcon& icon);
     static void drawIconGlyph(QPainter* painter, const QRect& rect, const QChar& icon, const QColor& color);
     static void drawText(QPainter* painter, const QRect& rect, const QString& text, const QFont& font, const QColor& color);
     static void drawActionButtons(QPainter* painter, const QStyleOptionViewItem& option, const QRect& rect, const QVector<Action>& actions, const QColor& color,
-                                  int currentActionIndex = -1);
+                                  const int& currentActionIndex = 0, const int& hoveredActionIndex = 0, const bool& isSelected = false,
+                                  const bool& isHovered = false);
 
     [[nodiscard]] static QRect getIconRect(const QRect& itemRect);
     [[nodiscard]] static QRect getTitleRect(const QRect& itemRect, const int& actionsCount);
     [[nodiscard]] static QRect getSubtitleRect(const QRect& itemRect, const int& actionsCount);
     [[nodiscard]] static QRect getActionsRect(const QRect& itemRect, const int& actionsCount);
+    [[nodiscard]] static int getActionButtonIndex(const QPoint& pos, const QRect& actionsRect, const int& actionCount);
 
-    int m_currentActionIndex = 0;
+    QAbstractItemView* m_view;
+
+    mutable int m_currentActionIndex = 0;
+    mutable int m_hoveredActionIndex = 0;
+
+signals:
+    void hideWindow();
 };
