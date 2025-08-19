e362b5521e9cc1ca52fe7f821cf2d7cdf7891c0f
georgel2020 <lingeorge2020@outlook.com>
2025-08-19 17:46:43 +0800

    Add Apps Search module to support app launch
    - Introduce `AppsSearch` module for searching installed apps
    - Parse app shortcuts from Windows Start Menu to generate default apps list
    - Implement query processing with keyword-based matching
    - Add `getShortcutPath` to resolve Windows shortcut targets
    - Update `.clang-format` to allow a single empty line for improved formatting consistency

---
+++ src/modules/AppsSearch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../common/IModule.h"
+
+class AppsSearch : public IModule
+{
+    Q_OBJECT
+
+public:
+    explicit AppsSearch(QObject* parent = nullptr);
+
+    [[nodiscard]] QString name() const override { return "Apps Search"; }
+    [[nodiscard]] QChar iconGlyph() const override { return QChar(0xe5c3); } // Apps.
+    [[nodiscard]] QJsonDocument defaultConfig() const override;
+    void query(const QString& text) override;
+
+private:
+    struct AppInfo
+    {
+        QString name;
+        QString path;
+        QVector<QString> keywords;
+    };
+
+    QVector<AppInfo> m_apps;
+
+    static bool getShortcutPath(const QString& shortcutPath, QString& targetPath);
+};
