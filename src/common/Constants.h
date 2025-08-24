a3076ae8da9475bc6e208ce68b56da560d211c18
georgel2020 <86717650+georgel2020@users.noreply.github.com>
2025-08-24 17:43:01 +0800

    Refactor theme configuration and handling
    - Add `ThemeManager::defaultConfig` to provide default theme settings
    - Remove hardcoded color constants from `Constants.h` and replace them with dynamic values stored in `Theme.json`
    - Update `ThemeManager::initTheme` to load theme configuration from `Theme.json`
    - Modify `README.md` to include theme configuration details and examples

--- src/common/Constants.h
+++ src/common/Constants.h
@@ -19,15 +19,3 @@ constexpr auto TITLE_FONT_SIZE = 14;
 constexpr auto SUBTITLE_FONT_SIZE = 12;
 constexpr auto ICON_SIZE = 24;
 constexpr auto BUTTON_SIZE = 32;
-
-// Colors.
-constexpr auto DEFAULT_BACK_LIGHT = "#e3eae7";
-constexpr auto DEFAULT_BACK_DARK = "#252b2a";
-constexpr auto ACTIVE_BACK_LIGHT = "#cae6df";
-constexpr auto ACTIVE_BACK_DARK = "#334b47";
-constexpr auto ACCENT_BACK_LIGHT = "#006b60";
-constexpr auto ACCENT_BACK_DARK = "#82d5c7";
-constexpr auto DEFAULT_TEXT_LIGHT = "#171d1b";
-constexpr auto DEFAULT_TEXT_DARK = "#dde4e1";
-constexpr auto ACCENT_TEXT_LIGHT = "#ffffff";
-constexpr auto ACCENT_TEXT_DARK = "#003731";
--- src/core/ThemeManager.cpp
+++ src/core/ThemeManager.cpp
@@ -1,18 +1,82 @@
 #include "ThemeManager.h"
 #include <QApplication>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include <QStyleHints>
 #include "../common/Constants.h"
+#include "ConfigManager.h"
 
-void ThemeManager::initTheme() { m_isDarkMode = QApplication::styleHints()->colorScheme() == Qt::ColorScheme::Dark; }
+void ThemeManager::initTheme()
+{
+    const QJsonDocument doc = ConfigManager::loadConfig("Theme.json", defaultConfig());
+    const QJsonObject rootObject = doc.object();
+    const QString theme = rootObject["theme"].toString();
+    if (theme == "auto")
+        m_isDarkMode = QApplication::styleHints()->colorScheme() == Qt::ColorScheme::Dark;
+    else if (theme == "light")
+        m_isDarkMode = false;
+    else if (theme == "dark")
+        m_isDarkMode = true;
+    else
+        m_isDarkMode = false;
+    const QJsonObject colorsObject = rootObject["colors"].toObject();
+    const QJsonObject lightObject = colorsObject["light"].toObject();
+    const QJsonObject darkObject = colorsObject["dark"].toObject();
+    m_defaultBackLight = lightObject["background"].toObject()["default"].toString();
+    m_defaultBackDark = darkObject["background"].toObject()["default"].toString();
+    m_activeBackLight = lightObject["background"].toObject()["active"].toString();
+    m_activeBackDark = darkObject["background"].toObject()["active"].toString();
+    m_accentBackLight = lightObject["background"].toObject()["accent"].toString();
+    m_accentBackDark = darkObject["background"].toObject()["accent"].toString();
+    m_defaultTextLight = lightObject["text"].toObject()["default"].toString();
+    m_defaultTextDark = darkObject["text"].toObject()["default"].toString();
+    m_accentTextLight = lightObject["text"].toObject()["accent"].toString();
+    m_accentTextDark = darkObject["text"].toObject()["accent"].toString();
+}
 
-QColor ThemeManager::defaultBackColor() { return QColor::fromString(m_isDarkMode ? DEFAULT_BACK_DARK : DEFAULT_BACK_LIGHT); }
-QColor ThemeManager::activeBackColor() { return QColor::fromString(m_isDarkMode ? ACTIVE_BACK_DARK : ACTIVE_BACK_LIGHT); }
-QColor ThemeManager::accentBackColor() { return QColor::fromString(m_isDarkMode ? ACCENT_BACK_DARK : ACCENT_BACK_LIGHT); }
-QColor ThemeManager::defaultTextColor() { return QColor::fromString(m_isDarkMode ? DEFAULT_TEXT_DARK : DEFAULT_TEXT_LIGHT); }
-QColor ThemeManager::accentTextColor() { return QColor::fromString(m_isDarkMode ? ACCENT_TEXT_DARK : ACCENT_TEXT_LIGHT); }
+QJsonDocument ThemeManager::defaultConfig()
+{
+    // clang-format off
+    const QJsonObject rootObject{
+        {"theme", "auto"},
+        {"colors", QJsonObject{
+            {"light", QJsonObject{
+                {"background", QJsonObject{
+                    {"default", "#e3eae7"},
+                    {"active",  "#cae6df"},
+                    {"accent",  "#006b60"}
+                }},
+                {"text", QJsonObject{
+                    {"default", "#171d1b"},
+                    {"accent",  "#ffffff"}
+                }}
+            }},
+            {"dark", QJsonObject{
+                {"background", QJsonObject{
+                    {"default", "#252b2a"},
+                    {"active",  "#334b47"},
+                    {"accent",  "#82d5c7"}
+                }},
+                {"text", QJsonObject{
+                    {"default", "#dde4e1"},
+                    {"accent",  "#003731"}
+                }}
+            }}
+        }}
+    };
+    // clang-format on
 
-QString ThemeManager::defaultBackColorHex() { return m_isDarkMode ? DEFAULT_BACK_DARK : DEFAULT_BACK_LIGHT; }
-QString ThemeManager::activeBackColorHex() { return m_isDarkMode ? ACTIVE_BACK_DARK : ACTIVE_BACK_LIGHT; }
-QString ThemeManager::accentBackColorHex() { return m_isDarkMode ? ACCENT_BACK_DARK : ACCENT_BACK_LIGHT; }
-QString ThemeManager::defaultTextColorHex() { return m_isDarkMode ? DEFAULT_TEXT_DARK : DEFAULT_TEXT_LIGHT; }
-QString ThemeManager::accentTextColorHex() { return m_isDarkMode ? ACCENT_TEXT_DARK : ACCENT_TEXT_LIGHT; }
+    return QJsonDocument(rootObject);
+}
+
+QColor ThemeManager::defaultBackColor() { return QColor::fromString(m_isDarkMode ? m_defaultBackDark : m_defaultBackLight); }
+QColor ThemeManager::activeBackColor() { return QColor::fromString(m_isDarkMode ? m_activeBackDark : m_activeBackLight); }
+QColor ThemeManager::accentBackColor() { return QColor::fromString(m_isDarkMode ? m_accentBackDark : m_accentBackLight); }
+QColor ThemeManager::defaultTextColor() { return QColor::fromString(m_isDarkMode ? m_defaultTextDark : m_defaultTextLight); }
+QColor ThemeManager::accentTextColor() { return QColor::fromString(m_isDarkMode ? m_accentTextDark : m_accentTextLight); }
+
+QString ThemeManager::defaultBackColorHex() { return m_isDarkMode ? m_defaultBackDark : m_defaultBackLight; }
+QString ThemeManager::activeBackColorHex() { return m_isDarkMode ? m_activeBackDark : m_activeBackLight; }
+QString ThemeManager::accentBackColorHex() { return m_isDarkMode ? m_accentBackDark : m_accentBackLight; }
+QString ThemeManager::defaultTextColorHex() { return m_isDarkMode ? m_defaultTextDark : m_defaultTextLight; }
+QString ThemeManager::accentTextColorHex() { return m_isDarkMode ? m_accentTextDark : m_accentTextLight; }
